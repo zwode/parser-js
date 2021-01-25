cbda49bfb1e98a741fa92734cc6ebd47269ca797
abiko <abiko2001@gmail.com>
2021-01-25 16:27:44 +0500

    updated

--- index.js
+++ index.js
@@ -2,35 +2,36 @@
 import axios from "axios"
 import pkg from 'node-html-parser';
 
-const { parse } = pkg
+const { parse } = pkg;
 
 console.log("starting\n");
 
-axios.get('https://i-teka.kz/nur-sultan/medicamentsalphabetically')
-	.then((response) => {
-		return response;
-	})
-	.then((data) => {
-		const root = data;
-		// const body = document.querySelector('body');
-		function recursy (element) {
-			element.childNodes.forEach(node => {
-				if (node.nodeName.match(/^H\d/)) {
-					console.log(node);
-				} else {
-					recursy(node);
-				}
-			})
-		}
-		recursy(root);
-		// console.log(root);
-	});
+const url = 'https://i-teka.kz/nur-sultan/medicamentsalphabetically'
 
+async function takeClassPage(url) {
+	await axios.get(url)
+		.then(async (res) => {
+			let $ = cheerio.load(res.data);
+			let ids = [];
+			const items = $('.list-item').length;
+			console.log(items)
+		})
+		.then((response) => {
+			return response;
+		})
+		.then((data) => {
+			const root = pkg.parse(data);
+			console.log(root, '\n');
+			console.log(root.querySelectorAll('.list-item'));
+		});
+}
+
+takeClassPage(url);
 
 // var x = document.getElementsByClassName("example");
 //
 // console.log(x);
-
+//
 // window.addEventListener('DOMContentLoaded', () => {
 // 	const body = document.querySelector('body');
 // 	let textNodes = [];
